4767d6184d52ed303715e44fe95c0f5f238bf8fd
Astraeus-Design <118458240+Astraeus-Design@users.noreply.github.com>
2023-04-02 12:05:09 +0100

    reverted change

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
 
-  //external: ['react-router-dom', 'react-bootstrap'],
+  external: ['react-router-dom', 'react-bootstrap'],
 });
